ca94d0f62c91fae91026f21354b624b367f0f515
Gustavo-Vieira-Unasp <fonseca_vieira@educadventista.org>
2025-09-11 21:48:24 -0300

    Group index test

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grupo - php</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Olá, Mundo!"
+        ?>
+    </h1>
+    <p>
+        <?php
+            echo "Gustavo Vieira"
+        ?>
+    </p>
+</body>
+</html>
