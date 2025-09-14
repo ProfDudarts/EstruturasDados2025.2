643034c5743254d993dbbba28aecd1aff6bed64f
epilihP <philipegcustodio@gmail.com>
2025-09-14 19:08:23 -0300

    00.1.0.0.0.0.0.004

---
+++ php/pilha-PHP/src/Pilha/pilha.php
@@ -0,0 +1,26 @@
+<?php
+
+class Pilha {
+    private $itens = [];
+
+    public function inserir($item) {
+        array_push($this->itens, $item);
+    }
+
+    public function remover() {
+        if ($this->estaVazia()) {
+            return null;
+        }
+        return array_pop($this->itens);
+    }
+
+    public function listar() {
+        foreach ($this->itens as $item) {
+            $item->imprimir();
+        }
+    }
+
+    public function estaVazia() {
+        return empty($this->itens);
+    }
+}
