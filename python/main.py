3e1a3e5fae98f3cbaf6e2bd3ad2a3eb2cafd002f
UCB - UNASP-EC - Eduardo Mendes <eduardo.mendes@unasp.edu.br>
2025-09-23 22:55:59 -0300

    implementa lista encadeada

--- python/main.py
+++ python/main.py
@@ -2,21 +2,22 @@ from src.No.no import No
 from src.Pilha.pilha import Pilha
 from src.Fila.fila import Fila
 from src.Models.cliente import Cliente
+from src.ListaEncadeada.listaEncadeada import ListaEncadeada
 
-def atender(f: Fila):
-    atendido = f.remover()
-    print("Atendido: ", end="\t")
-    atendido.imprimir()
-    print("----------------------")
+# def atender(f: Fila):
+#     atendido = f.remover()
+#     print("Atendido: ", end="\t")
+#     atendido.imprimir()
+#     print("----------------------")
 
-def posicao(f: Fila, id: int):
-    print("----------------------")
-    pos = f.obterPosicao(id)
-    if pos > 0:
-        print(f"Id {id} é o {pos}º da fila.")
-    else:
-        print(f"Id {id} não encontrado")
-    print("----------------------")
+# def posicao(f: Fila, id: int):
+#     print("----------------------")
+#     pos = f.obterPosicao(id)
+#     if pos > 0:
+#         print(f"Id {id} é o {pos}º da fila.")
+#     else:
+#         print(f"Id {id} não encontrado")
+#     print("----------------------")
 
 
 c1 = Cliente(123, "Dudarts")
@@ -24,20 +25,32 @@ c2 = Cliente(456, "Lucas Eduardo")
 c3 = Cliente(789, "Pedro Eduardo")
 c4 = Cliente(135, "Dalila Chaves")
 c5 = Cliente(246, "Sansão Eduardo")
-c6 = Cliente(357, "Eduardo Mendes")
+c6 = Cliente(358, "Dalila Chaves")
+c7 = Cliente(359, "Dalila Chaves")
+c8 = Cliente(240, "Sansão Eduardo")
+c9 = Cliente(347, "Dalila Chaves")
 
-fila = Fila()
-fila.inserir(No(c1))
-fila.inserir(No(c2))
-fila.inserir(No(c3))
-fila.listar()
-posicao(fila, 789)
+le = ListaEncadeada()
+le.inserir(No(c1))
+le.inserir(No(c2))
+le.inserir(No(c3))
+le.inserir(No(c4))
+le.inserir(No(c5))
+le.inserir(No(c6))
+le.inserir(No(c7))
+le.inserir(No(c8))
+le.inserir(No(c9))
 
-fila.listar()
-atender(fila)
-fila.inserir(No(c4))
-atender(fila)
-fila.inserir(No(c5))
-posicao(fila, 135)
-fila.inserir(No(c6))
-fila.listar()
+no = le.removerTodos("Dalila Chaves")
+pass
+# fila.listar()
+# posicao(fila, 789)
+
+# fila.listar()
+# atender(fila)
+# fila.inserir(No(c4))
+# atender(fila)
+# fila.inserir(No(c5))
+# posicao(fila, 135)
+# fila.inserir(No(c6))
+# fila.listar()
